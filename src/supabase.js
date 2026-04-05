be7b4f2e7cbdf99d20a969d2d2211862780b7408
beliz <weissbeliz@gmail.com>
2026-04-05 14:34:26 +0200

    login hinzugefuegt

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://zyeixswgxrwybolobnzn.supabase.co'
+const supabaseKey = 'DEIN_PUBLISHABLE_KEY_HIER'
+
+export const supabase = createClient(URL: https://zyeixswgxrwybolobnzn.supabase.co, sb_publishable_ikjsQdgDQ0tJlE-XKVydSA_9dMD7cvj)
